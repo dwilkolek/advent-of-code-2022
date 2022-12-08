b2a97ef64d45625ad4defe85c67912352d8b583b
Damian Wilkołek <dwilkolek@protonmail.com>
2022-12-08 01:51:55 +0100

    Day 7 - RUST 🦀

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -31,6 +31,7 @@ impl Sizeable for File {
 
 #[derive(Debug)]
 struct Directory {
+    name: String,
     parent: Option<Rc<RefCell<Directory>>>,
     children: Vec<Rc<RefCell<Directory>>>,
     files: Vec<File>,
@@ -39,6 +40,7 @@ struct Directory {
 impl Directory {
     fn root() -> Directory {
         Directory {
+            name: String::from("/"),
             parent: None,
             children: Vec::new(),
             files: Vec::new(),
@@ -48,8 +50,10 @@ impl Directory {
         self.children.push(child);
     }
 
-    fn new(parent: Rc<RefCell<Directory>>) -> Directory {
+    fn new(name: String, parent: Rc<RefCell<Directory>>) -> Directory {
+        println!("Creating dir {}, {} ", name, parent.as_ref().borrow().name);
         Directory {
+            name,
             parent: Some(parent),
             children: Vec::new(),
             files: Vec::new(),
@@ -58,32 +62,159 @@ impl Directory {
     fn add_file(&mut self, file: File) {
         self.files.push(file)
     }
+    fn get_children(&self, name: String) -> Option<Rc<RefCell<Directory>>> {
+        self.children
+            .clone()
+            .into_iter()
+            .find(|child| child.as_ref().borrow().name == name)
+    }
+
+    fn get_file(&self, name: String) -> Option<bool> {
+        match self
+            .files
+            .clone()
+            .into_iter()
+            .find(|child| child.name == name)
+        {
+            Some(_) => Some(true),
+            None => None,
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct File {
+    name: String,
     size: usize,
 }
 
 impl File {
-    fn new(size: usize) -> File {
-        File { size }
+    fn new(name: String, size: usize) -> File {
+        File { name, size }
     }
 }
 
 fn main() {
+    let mut directories: Vec<Rc<RefCell<Directory>>> = vec![];
     let root = Rc::new(RefCell::new(Directory::root()));
-    let current = Rc::clone(&root);
+    let mut current = Rc::clone(&root);
 
-    let child = Rc::new(RefCell::new(Directory::new(Rc::clone(&current))));
-    root.borrow_mut().add_child(Rc::clone(&child));
+    let mut path: Vec<String> = vec![];
+    if let Ok(lines) = read_lines("input.txt") {
+        let cmd_dump: Vec<String> = lines.into_iter().map(|l| l.unwrap()).collect();
 
-    child.as_ref().borrow_mut().add_file(File::new(100));
-    root.as_ref().borrow_mut().add_file(File::new(100));
-    root.as_ref().borrow_mut().add_file(File::new(100));
-    root.as_ref().borrow_mut().add_file(File::new(100));
+        for cmd in cmd_dump.into_iter() {
+            let cmd_parts: Vec<&str> = cmd.split(" ").into_iter().collect();
+            if cmd.starts_with("$") {
+                //command
 
-    println!("Size: {}", root.as_ref().borrow().size())
+                println!("{} # {}", path.join(" / "), cmd);
+                match cmd_parts[1] {
+                    "cd" => match cmd_parts[2] {
+                        ".." => {
+                            let current_clone = Rc::clone(&current);
+                            let parent = Rc::clone(
+                                current_clone
+                                    .as_ref()
+                                    .borrow()
+                                    .parent
+                                    .as_ref()
+                                    .expect("Parent is missing"),
+                            );
+                            path.pop();
+                            current = parent;
+                        }
+                        "/" => {
+                            current = {
+                                path = vec![];
+                                Rc::clone(&root)
+                            }
+                        }
+                        name => {
+                            path.push(name.to_owned());
+                            let already_exists = current.borrow_mut().get_children(name.to_owned());
+                            match already_exists {
+                                Some(child) => current = Rc::clone(&child),
+                                None => {
+                                    let child = Rc::new(RefCell::new(Directory::new(
+                                        String::from(name),
+                                        Rc::clone(&current),
+                                    )));
+                                    current.borrow_mut().add_child(Rc::clone(&child));
+                                    directories.push(Rc::clone(&child));
+                                    current = Rc::clone(&child)
+                                }
+                            }
+                        }
+                    },
+                    "ls" => {
+                        println!("NOOP LS")
+                    }
+                    _ => (),
+                }
+            } else {
+                println!("  --- {}", cmd);
+                if cmd_parts[0] == "dir" {
+                    let name = cmd_parts[1];
+                    let already_exists = current.borrow_mut().get_children(name.to_owned());
+                    match already_exists {
+                        None => {
+                            let child = Rc::new(RefCell::new(Directory::new(
+                                String::from(name),
+                                Rc::clone(&current),
+                            )));
+                            directories.push(Rc::clone(&child));
+                            current.borrow_mut().add_child(Rc::clone(&child));
+                        }
+                        _ => {
+                            println!("NOOP")
+                        }
+                    }
+                } else {
+                    let size: usize = cmd_parts[0].to_string().parse().unwrap();
+                    let name = String::from(cmd_parts[1]);
+                    let has_file = current.as_ref().borrow().get_file(name.clone());
+                    match has_file {
+                        None => {
+                            current
+                                .as_ref()
+                                .borrow_mut()
+                                .add_file(File::new(name, size));
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    let below_100000: usize = directories
+        .clone()
+        .into_iter()
+        .filter(|dir| dir.as_ref().borrow().size() < 100000)
+        .map(|dir| dir.as_ref().borrow().size())
+        .sum();
+
+    println!("Total of directories below 100000: {}", below_100000);
+
+    //Part 2
+    let total_disk_space = 70000000;
+    let free_required = 30000000;
+    let unused = total_disk_space - root.as_ref().borrow().size();
+    let to_delete = free_required - unused;
+    let mut matching_dirs_sizes: Vec<usize> = directories
+        .clone()
+        .into_iter()
+        .filter(|dir| dir.as_ref().borrow().size() >= to_delete)
+        .map(|dir| dir.as_ref().borrow().size())
+        .collect();
+    matching_dirs_sizes.sort();
+    matching_dirs_sizes.first().unwrap();
+
+    println!(
+        "Should delete directory of size: {}",
+        matching_dirs_sizes.first().unwrap()
+    );
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<std::fs::File>>>
