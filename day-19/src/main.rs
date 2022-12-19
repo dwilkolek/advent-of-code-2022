d2213d1d503eadd8b21b8f14eebe4cf41cec0e15
Damian Wilkołek <damian.wilkolek@technipfmc.com>
2022-12-20 00:34:07 +0100

    Day 19

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -1,341 +1,165 @@
 // #![allow(dead_code)]
 // #![allow(unused_variables)]
 // #![allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-use std::fmt::Display;
-use std::io::{self, BufRead};
-use std::path::Path;
-use std::sync::mpsc;
-use std::{thread, vec};
 
 use regex::Regex;
+use std::collections::{HashSet, VecDeque};
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::vec;
 
 #[derive(Hash, PartialEq, Eq, Debug, Clone)]
-enum Mat {
-    Ore,
-    Clay,
-    Obsidian,
-    Geode,
-}
-impl Display for Mat {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Mat::Clay => write!(f, "Clay"),
-            Mat::Obsidian => write!(f, "Obsidian"),
-            Mat::Ore => write!(f, "Ore"),
-            Mat::Geode => write!(f, "Geode"),
-        }
-    }
-}
-const MATERIAL_ORDER: [Mat; 4] = [Mat::Geode, Mat::Obsidian, Mat::Clay, Mat::Ore];
-
-#[derive(Debug, Clone)]
-struct Robot {
-    cost: Vec<(Mat, usize)>,
-    material: Mat,
-    active: bool,
-}
-
-#[derive(Debug, Clone)]
 struct Blueprint {
-    id: usize,
-    stash: HashMap<Mat, usize>,
-    robot_blueprints: HashMap<Mat, Robot>,
-    robots: Vec<Robot>,
-    time_left: usize,
-    log: Vec<String>,
-    first_g: usize,
-    first_o: usize,
-    first_c: usize,
+    id: i32,
+    ore_robot_cost: i32,
+    clay_robot_cost: i32,
+    obsidian_robot_cost: (i32, i32),
+    geode_robot_cost: (i32, i32),
 }
 
-impl Blueprint {
-    fn quality(&self) -> usize {
-        match self.stash.get(&Mat::Geode) {
-            Some(count) => count * self.id,
-            None => 0 * self.id,
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+struct State {
+    time: i32,
+    ore: i32,
+    ore_robots: i32,
+    clay: i32,
+    clay_robots: i32,
+    obsidian: i32,
+    obsidian_robots: i32,
+    geode: i32,
+    geode_robots: i32,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            time: 0,
+            ore: 0,
+            ore_robots: 1,
+            clay: 0,
+            clay_robots: 0,
+            obsidian: 0,
+            obsidian_robots: 0,
+            geode: 0,
+            geode_robots: 0,
         }
     }
 
-    fn robots(&self, m: Mat) -> usize {
-        self.robots.iter().filter(|r| r.material == m).count()
-    }
-
-    fn materials_stashed() {
-
-        // self.stash
-        //     .clone()
-        //     .into_iter()
-        //     .map(|s| match s {
-        //         (Mat::Ore, c) => c,
-        //         (Mat::Clay, c) => 1000 * c,
-        //         (Mat::Obsidian, c) => 1000 * c,
-        //         (Mat::Geode, c) => 10000 * c,
-        //     })
-        //     .sum(),
-    }
-
-    fn unique_id(
-        &self,
-    ) -> (
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-    ) {
-        (
-            self.id,
-            self.time_left,
-            *self.stash.get(&Mat::Ore).unwrap_or(&0),
-            *self.stash.get(&Mat::Clay).unwrap_or(&0),
-            *self.stash.get(&Mat::Obsidian).unwrap_or(&0),
-            *self.stash.get(&Mat::Geode).unwrap_or(&0),
-            self.robots(Mat::Ore),
-            self.robots(Mat::Clay),
-            self.robots(Mat::Obsidian),
-            self.robots(Mat::Geode),
-        )
-    }
-}
-
-fn create_robots(blueprint: &Blueprint) -> Vec<Blueprint> {
-    let mut alternative_realities = vec![];
-    alternative_realities.push(blueprint.clone()); //noop
-
-    for mat in MATERIAL_ORDER {
-        let rb = blueprint.robot_blueprints.get(&mat).unwrap();
-        let mut can_produce = true;
-        for mat_cost in rb.cost.iter() {
-            can_produce &= blueprint.stash.get(&mat_cost.0).unwrap() >= &mat_cost.1;
-        }
-        if can_produce {
-            let mut producing = blueprint.clone();
-            producing.robots.push(rb.clone());
-            for mat_cost in rb.cost.iter() {
-                match producing.stash.get(&mat_cost.0) {
-                    Some(count) => {
-                        producing
-                            .stash
-                            .insert(mat_cost.0.clone(), count - mat_cost.1);
-                    }
-                    None => {
-                        panic!("Not enough materials")
-                    }
-                }
-            }
-            producing.log.push(format!(
-                "Creating {} robot. Timeleft: {}",
-                &mat, &producing.time_left
-            ));
-            alternative_realities.push(producing);
-        }
-    }
-    alternative_realities.sort_by(|a, b| b.robots(Mat::Geode).cmp(&a.robots(Mat::Geode)));
-    alternative_realities
-}
-
-fn collect_mats(blueprint: &mut Blueprint) {
-    for r in blueprint.robots.iter() {
-        if r.active {
-            if r.material == Mat::Geode {
-                blueprint.first_g = blueprint.first_g.min(blueprint.time_left)
-            }
-            blueprint.log.push(format!("Collected {}.", &r.material));
-            match blueprint.stash.get(&r.material) {
-                Some(count) => {
-                    blueprint.stash.insert(r.material.clone(), count + 1);
-                }
-                None => {
-                    blueprint.stash.insert(r.material.clone(), 1);
-                }
-            }
-        }
-    }
-}
-fn activate_robots(blueprint: &mut Blueprint) {
-    blueprint.time_left -= 1;
-
-    blueprint.log.push(format!("Stash {:?}", blueprint.stash));
-    blueprint
-        .log
-        .push(format!("== Minute {} ==", 25 - blueprint.time_left));
-
-    for r in blueprint.robots.iter_mut() {
-        r.active = true;
-    }
-}
-
-fn find_best(
-    mut blueprint: Blueprint,
-    cache: &mut HashSet<(
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-        usize,
-    )>,
-    first_g: &mut HashMap<usize, usize>,
-    first_o: &mut HashMap<usize, usize>,
-    first_c: &mut HashMap<usize, usize>,
-) -> Option<Blueprint> {
-    cache.insert(blueprint.unique_id());
-
-    if blueprint.first_g >= *first_g.get(&blueprint.id).unwrap_or(&0) {
-        first_g.insert(blueprint.id, blueprint.first_g);
-    } else {
-        return None;
-    }
-    // if blueprint.first_o >= *first_o.get(&blueprint.id).unwrap_or(&0) {
-    //     first_o.insert(blueprint.id, blueprint.first_o);
-    // } else {
-    //     return None;
-    // }
-    // if blueprint.first_c >= *first_c.get(&blueprint.id).unwrap_or(&0) {
-    //     first_c.insert(blueprint.id, blueprint.first_c);
-    // } else {
-    //     return None;
-    // }
-
-    collect_mats(&mut blueprint);
-    activate_robots(&mut blueprint);
-    if blueprint.time_left != 0 {
-        let paths = create_robots(&blueprint);
-        let mut best = blueprint;
-        for path in paths {
-            if !cache.contains(&path.unique_id()) {
-                let opportunity = find_best(path, cache, first_g, first_o, first_c);
-                if let Some(opportunity) = opportunity {
-                    if opportunity.quality() > best.quality() {
-                        best = opportunity;
-                    }
-                }
-            }
-        }
-        Some(best)
-    } else {
-        Some(blueprint)
+    fn collect(&mut self) {
+        self.time += 1;
+        self.ore += self.ore_robots;
+        self.clay += self.clay_robots;
+        self.obsidian += self.obsidian_robots;
+        self.geode += self.geode_robots;
     }
 }
 
 fn main() {
     let regex = Regex::new("Blueprint ([0-9]+): Each ore robot costs ([0-9]+) ore. Each clay robot costs ([0-9]+) ore. Each obsidian robot costs ([0-9]+) ore and ([0-9]+) clay. Each geode robot costs ([0-9]+) ore and ([0-9]+) obsidian.").unwrap();
-
     let mut blueprints: Vec<Blueprint> = vec![];
     if let Ok(lines) = read_lines("input.txt") {
         let input: Vec<String> = lines.into_iter().map(|l| l.unwrap()).collect();
         for line in input.into_iter() {
             let cap = regex.captures(&line).unwrap();
-            let mut robot_blueprints = HashMap::new();
-            robot_blueprints.insert(
-                Mat::Ore,
-                Robot {
-                    active: false,
-                    material: Mat::Ore,
-                    cost: vec![(Mat::Ore, cap.get(2).unwrap().as_str().parse().unwrap())],
-                },
-            );
-            robot_blueprints.insert(
-                Mat::Clay,
-                Robot {
-                    active: false,
-                    material: Mat::Clay,
-                    cost: vec![(Mat::Ore, cap.get(3).unwrap().as_str().parse().unwrap())],
-                },
-            );
-            robot_blueprints.insert(
-                Mat::Obsidian,
-                Robot {
-                    active: false,
-                    material: Mat::Obsidian,
-                    cost: vec![
-                        (Mat::Ore, cap.get(4).unwrap().as_str().parse().unwrap()),
-                        (Mat::Clay, cap.get(5).unwrap().as_str().parse().unwrap()),
-                    ],
-                },
-            );
-            robot_blueprints.insert(
-                Mat::Geode,
-                Robot {
-                    active: false,
-                    material: Mat::Geode,
-                    cost: vec![
-                        (Mat::Ore, cap.get(6).unwrap().as_str().parse().unwrap()),
-                        (Mat::Obsidian, cap.get(7).unwrap().as_str().parse().unwrap()),
-                    ],
-                },
-            );
-
-            let mut stash = HashMap::new();
-            stash.insert(Mat::Clay, 0);
-            stash.insert(Mat::Geode, 0);
-            stash.insert(Mat::Obsidian, 0);
-            stash.insert(Mat::Ore, 0);
             blueprints.push(Blueprint {
-                time_left: 25,
                 id: cap.get(1).unwrap().as_str().parse().unwrap(),
-                stash,
-                robot_blueprints,
-                log: vec![],
-                first_g: 0,
-                first_o: 0,
-                first_c: 0,
-                robots: vec![Robot {
-                    active: false,
-                    cost: vec![],
-                    material: Mat::Ore,
-                }],
+                ore_robot_cost: cap.get(2).unwrap().as_str().parse().unwrap(),
+                clay_robot_cost: cap.get(3).unwrap().as_str().parse().unwrap(),
+                obsidian_robot_cost: (
+                    cap.get(4).unwrap().as_str().parse().unwrap(),
+                    cap.get(5).unwrap().as_str().parse().unwrap(),
+                ),
+                geode_robot_cost: (
+                    cap.get(6).unwrap().as_str().parse().unwrap(),
+                    cap.get(7).unwrap().as_str().parse().unwrap(),
+                ),
             });
         }
     }
-
-    let (tx, rx) = mpsc::channel();
-
-    let mut handlers = vec![];
-    for blueprint in blueprints {
-        let tx = tx.clone();
-        let h = thread::spawn(move || {
-            println!("Started Blueprint {}", blueprint.id);
-            let mut cache = HashSet::new();
-            let mut first_g = HashMap::new();
-            let mut first_o = HashMap::new();
-            let mut first_c = HashMap::new();
-            let quality = find_best(
-                blueprint.clone(),
-                &mut cache,
-                &mut first_g,
-                &mut first_o,
-                &mut first_c,
-            )
-            .unwrap()
-            .quality();
-            // result += quality;
-            println!("Blueprint {} done, quality {}", blueprint.id, quality);
-            tx.send(quality).unwrap();
-        });
-        handlers.push(h)
+    let mut part1 = 0;
+    for blueprint in blueprints.iter() {
+        println!("Blueprint {} ", blueprint.id);
+        part1 += test(blueprint, 24) * blueprint.id;
     }
 
-    for h in handlers {
-        h.join().unwrap()
-    }
-    let mut result = 0;
-    for received in rx {
-        println!("Got: {}", received);
-        result += received;
+    println!("Part 1: {}", part1);
+
+    let mut part2 = 1;
+    for blueprint in blueprints[0..3].to_vec().iter() {
+        println!("Blueprint {} ", blueprint.id);
+        part2 *= test(blueprint, 32);
     }
 
-    println!("Results: {:?}", result);
+    println!("Part 2: {}", part2);
+}
+
+fn test(blueprint: &Blueprint, time_limit: i32) -> i32 {
+    let mut uniqe_states = HashSet::new();
+    let mut deq: VecDeque<State> = VecDeque::new();
+    let mut best = 0;
+
+    deq.push_back(State::new());
+
+    while let Some(mut state) = deq.pop_front() {
+        if state.geode > best {
+            best = state.geode
+        }
+        if state.geode < best - 1 || uniqe_states.contains(&state) {
+            continue;
+        };
+        uniqe_states.insert(state);
+
+        if state.time == time_limit {
+            continue;
+        }
+
+        if state.ore >= blueprint.geode_robot_cost.0
+            && state.obsidian >= blueprint.geode_robot_cost.1
+        {
+            let mut next = State {
+                ore: state.ore - blueprint.geode_robot_cost.0,
+                obsidian: state.obsidian - blueprint.geode_robot_cost.1,
+                ..state
+            };
+            next.collect();
+            next.geode_robots += 1;
+            deq.push_back(next);
+        }
+        if blueprint.obsidian_robot_cost.0 <= state.ore
+            && blueprint.obsidian_robot_cost.1 <= state.clay
+        {
+            let mut next = State {
+                ore: state.ore - blueprint.obsidian_robot_cost.0,
+                clay: state.clay - blueprint.obsidian_robot_cost.1,
+                ..state
+            };
+            next.collect();
+            next.obsidian_robots += 1;
+            deq.push_back(next);
+        }
+        if blueprint.ore_robot_cost <= state.ore {
+            let mut next = State {
+                ore: state.ore - blueprint.ore_robot_cost,
+                ..state
+            };
+            next.collect();
+            next.ore_robots += 1;
+            deq.push_back(next);
+        }
+        if blueprint.clay_robot_cost <= state.ore {
+            let mut next = State {
+                ore: state.ore - blueprint.clay_robot_cost,
+                ..state
+            };
+            next.collect();
+            next.clay_robots += 1;
+            deq.push_back(next);
+        }
+        state.collect();
+        deq.push_back(state);
+    }
+
+    best
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<std::fs::File>>>
